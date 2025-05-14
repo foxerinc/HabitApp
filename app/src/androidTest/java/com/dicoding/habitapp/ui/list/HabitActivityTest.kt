7db5d899b6b3af3cabd1aa8479606b9149254cca
foxerinc <78334731+foxerinc@users.noreply.github.com>
2025-05-14 09:25:59 +0700

    Update HabitActivityTest.kt

--- app/src/androidTest/java/com/dicoding/habitapp/ui/list/HabitActivityTest.kt
+++ app/src/androidTest/java/com/dicoding/habitapp/ui/list/HabitActivityTest.kt
@@ -1,6 +1,35 @@
 package com.dicoding.habitapp.ui.list
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.dicoding.habitapp.R
+import com.dicoding.habitapp.ui.add.AddHabitActivity
+
+import org.junit.Rule
+import org.junit.Test
+
 //TODO 16 : Write UI test to validate when user tap Add Habit (+), the AddHabitActivity displayed
 class HabitActivityTest {
+
+    @get:Rule
+    var scenarioRule = ActivityScenarioRule(HabitListActivity::class.java)
+
+    @Test
+    fun validateAddHabitActivity(){
+        Intents.init()
+        onView(withId(R.id.fab)).perform(click())
+        Intents.intended(hasComponent(AddHabitActivity::class.java.name))
+        onView(withId(R.id.add_ed_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.add_ed_minutes_focus)).check(matches(isDisplayed()))
+        onView(withId(R.id.sp_priority_level)).check(matches(isDisplayed()))
+        onView(withId(R.id.add_tv_start_time)).check(matches(isDisplayed()))
+        Intents.release()
+    }
 
 }
